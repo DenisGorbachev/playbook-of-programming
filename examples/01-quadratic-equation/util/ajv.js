ab8aee0441796f395e885501180913ca9e6dd491
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2020-12-26 20:16:43 +0700

    Fix ajv

--- examples/01-quadratic-equation/util/ajv.js
+++ examples/01-quadratic-equation/util/ajv.js
@@ -3,9 +3,17 @@ import path from 'path'
 
 export const ajv = new Ajv({ $data: true })
 
+export const getSharedBeginning = function (a1, a2) {
+  let i = 0
+  while (i < a1.length && a1.charAt(i) === a2.charAt(i)) i++
+  return a1.substring(0, i)
+}
+
 export const getSchemaId = function (filename) {
   const { dir, name } = path.parse(filename)
-  const relativeDir = dir.replace(__dirname + '/', '')
-  const nameFirstPart = name.split('.')[0]
-  return `https://playbook.io/schemas/${relativeDir}/${nameFirstPart}`
+  const projectDir = path.resolve(__dirname + '/..')
+  const relativeDir = dir.replace(projectDir + '/', '')
+  const nameWithoutExtension = name.split('.')[0]
+  const projectName = projectDir.split(path.sep).pop()
+  return `https://${projectName}/schemas/${relativeDir}/${nameWithoutExtension}`
 }
