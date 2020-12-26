b5cc07024be284d98d0180307364d72a7cb90575
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2020-12-26 20:17:03 +0700

    Remove unused code

--- examples/01-quadratic-equation/util/ajv.js
+++ examples/01-quadratic-equation/util/ajv.js
@@ -3,12 +3,6 @@ import path from 'path'
 
 export const ajv = new Ajv({ $data: true })
 
-export const getSharedBeginning = function (a1, a2) {
-  let i = 0
-  while (i < a1.length && a1.charAt(i) === a2.charAt(i)) i++
-  return a1.substring(0, i)
-}
-
 export const getSchemaId = function (filename) {
   const { dir, name } = path.parse(filename)
   const projectDir = path.resolve(__dirname + '/..')
