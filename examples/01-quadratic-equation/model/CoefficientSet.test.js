b5d87fcad4d87bc240bd80700d4b104c30be476d
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2020-12-28 10:26:39 +0700

    Fix typo

--- examples/01-quadratic-equation/model/CoefficientSet.test.js
+++ examples/01-quadratic-equation/model/CoefficientSet.test.js
@@ -1,6 +1,6 @@
 import { CoefficientSetSamples, validateCoefficientSet } from './CoefficientSet.js'
 
-test('CoefficentSet samples match schema', async function() {
+test('CoefficientSet samples match schema', async function() {
   for (const sample of CoefficientSetSamples) {
     const value = await validateCoefficientSet(sample)
     expect(value).toBeDefined()
