45b353bbcd821b0f230a6d6a878ccdecc4b83a49
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2021-01-18 10:48:21 +0700

    Refactor tests

--- examples/02-bookshelf/model/Author.test.js
+++ examples/02-bookshelf/model/Author.test.js
@@ -1,9 +1,13 @@
 import { AuthorValidSamples, AuthorInvalidSamples, getAuthorErrors } from './Author.js'
 
-test.each(AuthorValidSamples)('Author valid sample matches schema \n%o', async function (sample) {
-  await expect(getAuthorErrors(sample)).resolves.toEqual([])
+test.each(AuthorValidSamples)('Author valid sample matches schema', async function (sample) {
+  const errors = await getAuthorErrors(sample)
+  if (errors.length) console.warn(sample)
+  await expect(errors).toEqual([])
 })
 
-test.each(AuthorInvalidSamples)('Author invalid sample does not match schema \n%o', async function (sample) {
-  await expect(getAuthorErrors(sample)).resolves.toContainEqual(expect.any(Object))
+test.each(AuthorInvalidSamples)('Author invalid sample does not match schema', async function (sample) {
+  const errors = await getAuthorErrors(sample)
+  if (!errors.length) console.warn(sample)
+  await expect(errors).not.toEqual([])
 })
--- examples/02-bookshelf/model/Book.test.js
+++ examples/02-bookshelf/model/Book.test.js
@@ -1,9 +1,13 @@
 import { BookValidSamples, BookInvalidSamples, getBookErrors } from './Book.js'
 
-test.each(BookValidSamples)('Book valid sample matches schema \n%o', async function (sample) {
-  await expect(getBookErrors(sample)).resolves.toEqual([])
+test.each(BookValidSamples)('Book valid sample matches schema', async function (sample) {
+  const errors = await getBookErrors(sample)
+  if (errors.length) console.warn(sample)
+  await expect(errors).toEqual([])
 })
 
-test.each(BookInvalidSamples)('Book invalid sample does not match schema \n%o', async function (sample) {
-  await expect(getBookErrors(sample)).resolves.toContainEqual(expect.any(Object))
+test.each(BookInvalidSamples)('Book invalid sample does not match schema', async function (sample) {
+  const errors = await getBookErrors(sample)
+  if (!errors.length) console.warn(sample)
+  await expect(errors).not.toEqual([])
 })
